d51d22e98dd15157dbaad9a56a993342a99f2af0
FTS427 <FTS427@outlook.com>
2023-07-01 13:58:55 +0800

    Update

--- ECPPH.h
+++ ECPPH.h
@@ -6,11 +6,24 @@ void error(){
     printf("\033[1,31mERROR!\033[0m","\n");
 }
 
-void clear(int i){
-    if(i==0)
+void clear(bool i){
+    if(i==false)
         system("clear");
-    else if(i==1)
+    if(i==true)
         system("CLS");
-    else
-        error();
+}
+
+void print(const char a,char color[]={"RED","YELLOW","ORINGE","BLUE","GREEN","WHITE"},bool i){
+    if(i==false){
+        if(color="RED")
+            printf("\033[31m"+a+"\033[0m")
+        if(color="RED")
+            printf("\033[31m"+a+"\033[0m");
+    }
+    if(i==true){
+        if(color="RED")
+            printf("\033[31m"+a+"\033[0m")
+        if(color="RED")
+            printf("\033[31m"+a+"\033[0m");
+    }
 }

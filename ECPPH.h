d044c6f7b2347fdf67e279e933954493da46efbb
FTS427 <FTS427@outlook.com>
2023-07-01 20:08:42 +0800

    Update

--- ECPPH.h
+++ ECPPH.h
@@ -1,34 +1,61 @@
 #include <iostream>
-
+using namespace std;
 //Easy C++ Head
 
 void error(){
-    printf("\033[1,31mERROR!\033[0m\a\n");
+    cout<<"\033[1,31mERROR!\033[0m\a\n";
 }
 
-void clear(bool i){
-    if(i==false)
+void clear(int i){
+    if(i=='0')
         system("clear");
-    if(i==true)
+    if(i=='1')
         system("CLS");
 }
 
-void print(const char a[],in b,bool i){
+void print(const char T[],int color,bool i){
     if(i==true){
-        if(b==0)
-            printf("\033[0m"+a+"\033[0m\n");
-        if(b==1)
-            printf("\033[31m"+a+"\033[0m\n");
-        if(b==2)
-            printf("\033[32m"+a+"\033[0m\n");
+        if(color==0)
+            cout<<"\033[0m"<<T<<"\033[0m\n";
+        if(color==1)
+            cout<<"\033[31m"<<T<<"\033[0m\n";
+        if(color==2)
+            cout<<"\033[33m"<<T<<"\033[0m\n";
+        if(color==3)
+            cout<<"\033[32m"<<T<<"\033[0m\n";
+        if(color==4)
+            cout<<"\033[36m"<<T<<"\033[0m\n";
+        if(color==5)
+            cout<<"\033[34m"<<T<<"\033[0m\n";
+        if(color==6)
+            cout<<"\033[35m"<<T<<"\033[0m\n";
     }
     if(i==false){
-    if(i==true){
-        if(b==0)
-            printf("\033[0m"+a+"\033[0m");
-        if(b==1)
-            printf("\033[31m"+a+"\033[0m");
-        if(b==2)
-            printf("\033[32m"+a+"\033[0m");
+        if(color==0)
+            cout<<"\033[0m"<<T<<"\033[0m";
+        if(color==1)
+            cout<<"\033[31m"<<T<<"\033[0m";
+        if(color==2)
+            cout<<"\033[33m"<<T<<"\033[0m";
+        if(color==3)
+            cout<<"\033[32m"<<T<<"\033[0m";
+        if(color==4)
+            cout<<"\033[36m"<<T<<"\033[0m";
+        if(color==5)
+            cout<<"\033[34m"<<T<<"\033[0m";
+        if(color==6)
+            cout<<"\033[35m"<<T<<"\033[0m";
+    }
+}
+
+void h(const char S[],int l,int c){
+    if(l<=0)
+        error();
+    else{
+        while(l>1){
+            print(S,c,false);
+            l--;
+        }
+        print(S,c,true);
     }
 }
---
+++ example.cpp
@@ -0,0 +1,12 @@
+#include "ECPPH.h"
+
+int main(){
+    clear(0);
+    h("*",5,5);
+    int a;
+    cin>>a;
+    print("Hello",1,false);
+    print("World!",6,true);
+    error();
+    return 0;
+}

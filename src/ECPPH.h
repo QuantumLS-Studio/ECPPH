da90e8d64db4a00ba343e8c1315f5b333980d4d7
FTS427 <FTS427@outlook.com>
2023-07-02 10:05:55 +0800

    Uptate

--- src/ECPPH.h
+++ src/ECPPH.h
@@ -1,51 +1,65 @@
 #include <iostream>
+#include <cmath>
+#include <limits>
 using namespace std;
 //Easy C++ Head
 
 void error(){
-    cout<<"\033[1,31mERROR!\033[0m\a\n";
+    cout<<"\033[1,31m"<<"ERROR!"<<"\033[0m\a\n";
 }
 
 void clear(int i){
     if(i=='0')
         system("clear");
-    if(i=='1')
+    else if(i=='1')
         system("CLS");
+    else
+        error();
 }
 
 void print(const char T[],int color,bool i){
     if(i==true){
         if(color==0)
-            cout<<"\033[0m"<<T<<"\033[0m\n";
-        if(color==1)
+            cout<<T<<"\n";
+        else if(color==1)
             cout<<"\033[31m"<<T<<"\033[0m\n";
-        if(color==2)
+        else if(color==2)
             cout<<"\033[33m"<<T<<"\033[0m\n";
-        if(color==3)
+        else if(color==3)
             cout<<"\033[32m"<<T<<"\033[0m\n";
-        if(color==4)
+        else if(color==4)
             cout<<"\033[36m"<<T<<"\033[0m\n";
-        if(color==5)
+        else if(color==5)
             cout<<"\033[34m"<<T<<"\033[0m\n";
-        if(color==6)
+        else if(color==6)
             cout<<"\033[35m"<<T<<"\033[0m\n";
+        else if(color==7)
+            cout<<"\033[0m"<<T<<"\033[0m\n";
+        else
+            error();
     }
-    if(i==false){
+    else if(i==false){
         if(color==0)
-            cout<<"\033[0m"<<T<<"\033[0m";
-        if(color==1)
+            cout<<T;
+        else if(color==1)
             cout<<"\033[31m"<<T<<"\033[0m";
-        if(color==2)
+        else if(color==2)
             cout<<"\033[33m"<<T<<"\033[0m";
-        if(color==3)
+        else if(color==3)
             cout<<"\033[32m"<<T<<"\033[0m";
-        if(color==4)
+        else if(color==4)
             cout<<"\033[36m"<<T<<"\033[0m";
-        if(color==5)
+        else if(color==5)
             cout<<"\033[34m"<<T<<"\033[0m";
-        if(color==6)
+        else if(color==6)
             cout<<"\033[35m"<<T<<"\033[0m";
+        else if(color==7)
+            cout<<"\033[0m"<<T<<"\033[0m";
+        else
+            error();
     }
+    else
+        error();
 }
 
 void h(const char S[],int l,int c){
